faafef37255597031a272cf7aa372834fa571dd9
Anthony Larios <simpsonsruleA52@gmail.com>
2015-07-21 19:33:15 -0700

    add comments to the bubble_sort project

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,25 +1,35 @@
 def bubble_sort(array)
+	#Create a variable for the last number
 	right_marker = array.length - 1
+
 	until right_marker == 1
+		#You stop before the last number since it was previously compared when
+		#left_marker was equal to right_marker - 1
 		for left_marker in (0...right_marker)
+			#Swap the values of the numbers if the left number is greater than the right
 			if array[left_marker] > array[left_marker + 1]
 				array[left_marker], array[left_marker + 1] = array[left_marker + 1], array[left_marker]
 			end
 		end
+		#Check if the array is sorted and ready to be returned
 		return array if sorted?(array)
+		#Continue sorting and ignore right number since it is now the largest number
 		right_marker -= 1
 	end
 	array
 end
 
+#Very similar to bubble_sort
 def bubble_sort_by(array)
 	right_marker = array.length - 1
 	until right_marker == 1
 		for left_marker in (0...right_marker)
+			#The only difference is the yield to accept a block for sorting
 			if yield(array[left_marker], array[left_marker + 1]) > 0
 				array[left_marker], array[left_marker + 1] = array[left_marker + 1], array[left_marker]
 			end
 		end
+		#Check if the array is sorted and ready to be returned
 		return array if sorted?(array)
 		right_marker -= 1
 	end
@@ -27,6 +37,7 @@ def bubble_sort_by(array)
 end
 
 def sorted?(array_to_check)
+	#Sort by numbers or by length if array is a string
 	if array_to_check[0].is_a? (String)
 		array_to_check.each_cons(2).all? { |left, right| left.length <=> right.length < 1}
 	else
@@ -35,6 +46,7 @@ def sorted?(array_to_check)
 end
 
 p bubble_sort([4,3,78,2,0,2])
+#The do/end block only works if stored in a variable. Using ruby 1.9.3p547
 incremental_sort = bubble_sort_by(["hi","hello","hey"]) do |left,right| 
 	right.length - left.length
 end
