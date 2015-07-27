8ecf731789e2f4572cfdc055140d048187084d0a
Anthony Larios <simpsonsruleA52@gmail.com>
2015-07-26 20:28:07 -0700

    add enumerable_methods project, up to my_all? def

---
+++ enumerable_methods.rb
@@ -0,0 +1,37 @@
+module Enumerable
+	def my_each
+		return self unless block_given?
+		for item in self
+			yield(item)
+		end
+	end
+
+	def my_each_with_index
+		return self unless block_given?
+		for index in 0...self.length
+			yield(self[index], index)
+		end
+	end
+
+	def my_select
+		return self unless block_given?
+		selected = []
+		for item in self
+			selected << item if yield(item)
+		end
+		return selected
+	end
+
+	def my_all?
+		if block_given?
+			for item in self
+				return false unless yield(item)
+			end
+		else
+			for item in self
+				return false unless item
+			end
+		end
+		return true
+	end
+end
