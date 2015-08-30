97a8f9262dd114bee3e7783585f18fecb3745669
Anthony Larios <simpsonsruleA52@gmail.com>
2015-08-30 12:34:13 -0700

    Modify indentations for each file

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,54 +1,54 @@
 def bubble_sort(array)
-	#Create a variable for the last number
-	right_marker = array.length - 1
+  #Create a variable for the last number
+  right_marker = array.length - 1
 
-	until right_marker == 1
-		#You stop before the last number since it was previously compared when
-		#left_marker was equal to right_marker - 1
-		for left_marker in (0...right_marker)
-			#Swap the values of the numbers if the left number is greater than the right
-			if array[left_marker] > array[left_marker + 1]
-				array[left_marker], array[left_marker + 1] = array[left_marker + 1], array[left_marker]
-			end
-		end
-		#Check if the array is sorted and ready to be returned
-		return array if sorted?(array)
-		#Continue sorting and ignore right number since it is now the largest number
-		right_marker -= 1
-	end
-	array
+  until right_marker == 1
+    #You stop before the last number since it was previously compared when
+    #left_marker was equal to right_marker - 1
+    for left_marker in (0...right_marker)
+      #Swap the values of the numbers if the left number is greater than the right
+      if array[left_marker] > array[left_marker + 1]
+        array[left_marker], array[left_marker + 1] = array[left_marker + 1], array[left_marker]
+      end
+    end
+    #Check if the array is sorted and ready to be returned
+    return array if sorted?(array)
+    #Continue sorting and ignore right number since it is now the largest number
+    right_marker -= 1
+  end
+  array
 end
 
 #Very similar to bubble_sort
 def bubble_sort_by(array)
-	right_marker = array.length - 1
-	until right_marker == 1
-		for left_marker in (0...right_marker)
-			#The only difference is the yield to accept a block for sorting
-			if yield(array[left_marker], array[left_marker + 1]) > 0
-				array[left_marker], array[left_marker + 1] = array[left_marker + 1], array[left_marker]
-			end
-		end
-		#Check if the array is sorted and ready to be returned
-		return array if sorted?(array)
-		right_marker -= 1
-	end
-	array
+  right_marker = array.length - 1
+  until right_marker == 1
+    for left_marker in (0...right_marker)
+      #The only difference is the yield to accept a block for sorting
+      if yield(array[left_marker], array[left_marker + 1]) > 0
+        array[left_marker], array[left_marker + 1] = array[left_marker + 1], array[left_marker]
+      end
+    end
+    #Check if the array is sorted and ready to be returned
+    return array if sorted?(array)
+    right_marker -= 1
+  end
+  array
 end
 
 def sorted?(array_to_check)
-	#Sort by numbers or by length if array is a string
-	if array_to_check[0].is_a? (String)
-		array_to_check.each_cons(2).all? { |left, right| left.length <=> right.length < 1}
-	else
-		array_to_check.each_cons(2).all? { |left, right| left <=> right < 1}
-	end
+  #Sort by numbers or by length if array is a string
+  if array_to_check[0].is_a? (String)
+    array_to_check.each_cons(2).all? { |left, right| left.length <=> right.length < 1}
+  else
+    array_to_check.each_cons(2).all? { |left, right| left <=> right < 1}
+  end
 end
 
 p bubble_sort([4,3,78,2,0,2])
 #The do/end block only works if stored in a variable. Using ruby 1.9.3p547
 incremental_sort = bubble_sort_by(["hi","hello","hey"]) do |left,right| 
-	right.length - left.length
+  right.length - left.length
 end
 p incremental_sort
 p bubble_sort_by(["hi","hello","hey"]) { |left,right| right.length - left.length }
--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,103 +1,103 @@
 module Enumerable
-	def my_each
-		return self unless block_given?
-		for item in self
-			yield(item)
-		end
-	end
+  def my_each
+    return self unless block_given?
+    for item in self
+      yield(item)
+    end
+  end
 
-	def my_each_with_index
-		return self unless block_given?
-		for index in 0...self.length
-			yield(self[index], index)
-		end
-	end
+  def my_each_with_index
+    return self unless block_given?
+    for index in 0...self.length
+      yield(self[index], index)
+    end
+  end
 
-	def my_select
-		return self unless block_given?
-		selected = []
-		for item in self
-			selected << item if yield(item)
-		end
-		return selected
-	end
+  def my_select
+    return self unless block_given?
+    selected = []
+    for item in self
+      selected << item if yield(item)
+    end
+    return selected
+  end
 
-	def my_all?
-		if block_given?
-			for item in self
-				return false unless yield(item)
-			end
-		else
-			for item in self
-				return false unless item
-			end
-		end
-		return true
-	end
+  def my_all?
+    if block_given?
+      for item in self
+        return false unless yield(item)
+      end
+    else
+      for item in self
+        return false unless item
+      end
+    end
+    return true
+  end
 
-	def my_any?
-		if block_given?
-			for item in self
-				return true if yield(item)
-			end
-		else
-			for item in self
-				return true if item
-			end
-		end
-		return false
-	end
+  def my_any?
+    if block_given?
+      for item in self
+        return true if yield(item)
+      end
+    else
+      for item in self
+        return true if item
+      end
+    end
+    return false
+  end
 
-	def my_none?
-		if block_given?
-			for item in self
-				return false if yield(item)
-			end
-		else
-			for item in self
-				return false if item
-			end
-		end
-		return true
-	end
+  def my_none?
+    if block_given?
+      for item in self
+        return false if yield(item)
+      end
+    else
+      for item in self
+        return false if item
+      end
+    end
+    return true
+  end
 
-	def my_count (object = nil)
-		count = 0
-		if block_given?
-			for item in self
-				count += 1 if yield(item)
-			end
-		elsif object.nil?
-			for item in self
-				count += 1
-			end
-		else
-			for item in self
-				count += 1 if item == object
-			end
-		end
-		return count
-	end
+  def my_count (object = nil)
+    count = 0
+    if block_given?
+      for item in self
+        count += 1 if yield(item)
+      end
+    elsif object.nil?
+      for item in self
+        count += 1
+      end
+    else
+      for item in self
+        count += 1 if item == object
+      end
+    end
+    return count
+  end
 
-	def my_map(&block)
-		return self unless block_given?
-		array = []
-		for item in self
-			array << block.call(item)
-		end
-		return array
-	end
+  def my_map(&block)
+    return self unless block_given?
+    array = []
+    for item in self
+      array << block.call(item)
+    end
+    return array
+  end
 
-	def my_inject(initial = nil)
-		total = initial.nil? ? self.first : initial
-		range = initial.nil? ? self.my_count - 1 : self.my_count
-		for item in self.last(range)
-			total = yield(total, item)
-		end
-		return total
-	end
+  def my_inject(initial = nil)
+    total = initial.nil? ? self.first : initial
+    range = initial.nil? ? self.my_count - 1 : self.my_count
+    for item in self.last(range)
+      total = yield(total, item)
+    end
+    return total
+  end
 end
 
 def multiply_els (array)
-	array.my_inject(1) { |product, number| product * number }
+  array.my_inject(1) { |product, number| product * number }
 end
